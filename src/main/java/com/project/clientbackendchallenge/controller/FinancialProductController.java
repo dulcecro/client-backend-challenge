e9cc878cf33f53254a47a6ae7e94d9e013bda3c3
dcanchari <dtcancharir@gmail.com>
2025-07-30 14:38:43 -0500

    Searching financial products by uniqueCodeClient

--- src/main/java/com/project/clientbackendchallenge/controller/FinancialProductController.java
+++ src/main/java/com/project/clientbackendchallenge/controller/FinancialProductController.java
@@ -1,13 +1,28 @@
 package com.project.clientbackendchallenge.controller;
 
+import com.project.clientbackendchallenge.model.FinancialProduct;
 import com.project.clientbackendchallenge.service.FinancialProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/challenge")
 public class FinancialProductController {
     @Autowired
     private FinancialProductService financialProductService;
+
+    @GetMapping("/financialProduct")
+    public List<FinancialProduct> listFinancialProducts() {
+        return financialProductService.listFinancialProducts();
+    }
+
+    @GetMapping("/financialProduct/{uniqueCodeClient}")
+    public List<FinancialProduct> listFinancialProductsByUniqueCode(@PathVariable String uniqueCodeClient) {
+        return financialProductService.listFinancialProductsByUniqueCode(uniqueCodeClient);
+    }
 }
--- src/main/java/com/project/clientbackendchallenge/repository/FinancialProductRepository.java
+++ src/main/java/com/project/clientbackendchallenge/repository/FinancialProductRepository.java
@@ -3,6 +3,9 @@ package com.project.clientbackendchallenge.repository;
 import com.project.clientbackendchallenge.model.FinancialProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FinancialProductRepository extends JpaRepository<FinancialProduct, Integer> {
 
+    List<FinancialProduct> findByIdClient_UniqueCode(String uniqueCodeClient);
 }
