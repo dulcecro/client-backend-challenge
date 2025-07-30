51592c9aea3e32179dd6d97eb6a615247f493e9c
dcanchari <dtcancharir@gmail.com>
2025-07-30 09:29:52 -0500

    Init commit

---
+++ src/test/java/com/project/clientbackendchallenge/ClientBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.clientbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBackendChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
